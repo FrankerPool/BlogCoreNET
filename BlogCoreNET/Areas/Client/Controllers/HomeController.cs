29b54e7e8cf6f03824ebb5a68554a0b8b65d7036
FrankerPool <39344039+FrankerPool@users.noreply.github.com>
2024-12-02 11:09:24 -0600

    Add: Proyect Areas and configurate cshtml

--- BlogCoreNET/Areas/Client/Controllers/HomeController.cs
+++ BlogCoreNET/Areas/Client/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using BlogCoreNET.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BlogCoreNET.Controllers
+namespace BlogCoreNET.Areas.Client.Controllers
 {
+    [Area("Client")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
