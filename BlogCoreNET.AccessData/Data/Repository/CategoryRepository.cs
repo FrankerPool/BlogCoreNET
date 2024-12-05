b80221755d4de27f691a371c61b000717adfddbe
FrankerPool <39344039+FrankerPool@users.noreply.github.com>
2024-12-05 15:13:36 -0600

    Add: Repository Categoory update

---
+++ BlogCoreNET.AccessData/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using BlogCoreNET.AccessData.Data.Repository.IRepository;
+using BlogCoreNET.Data;
+using BlogCoreNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCoreNET.AccessData.Data.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void update(Category category)
+        {
+            var objDb = _db.category.FirstOrDefault(s => s.Id == category.Id);
+            objDb.Name = category.Name;
+            objDb.Order = category.Order;
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ BlogCoreNET.AccessData/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlogCoreNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCoreNET.AccessData.Data.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void update(Category category);
+    }
+}
